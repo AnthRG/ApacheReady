c16cfd077b6d18eb883b1ebc8cf69374803cd32b
Anthony Rosario G. <112951262+AnthRG@users.noreply.github.com>
2025-05-23 11:18:04 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -21,29 +21,33 @@ curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 21.0.3-tem
 
+sudo apt-get update
+sudo apt-get install -y mysql-server
+sudo systemctl start mysql
+sudo systemctl enable mysql
+
 # Subiendo el servicio de Apache
 sudo service apache2 start
 
 # Clonando el proyecto
 cd ~/
-git clone https://github.com/AnthRG/ApacheReady.git
-cd ~/ApacheReady/proyecto-final
+cd ~/ArSpring
 
 # Configuración de VirtualHost
 sudo bash -c 'cat > /etc/apache2/sites-available/seguro.conf << EOF
 <VirtualHost *:80>
-    ServerName suslinks.sus.codes
-    Redirect permanent / https://suslinks.sus.codes
+    ServerName arblocks.do
+    Redirect permanent / https://arblocks.do
 </VirtualHost>
 
 <VirtualHost *:443>
-    ServerName suslinks.sus.codes
+    ServerName arblocks.do
 
     DocumentRoot /var/www/html
 
     SSLEngine on
-    SSLCertificateFile /etc/letsencrypt/live/suslinks.sus.codes/fullchain.pem
-    SSLCertificateKeyFile /etc/letsencrypt/live/suslinks.sus.codes/privkey.pem
+    SSLCertificateFile /etc/letsencrypt/live/arblocks.do/fullchain.pem
+    SSLCertificateKeyFile /etc/letsencrypt/live/arblocks.do/privkey.pem
 
     ProxyPass / http://localhost:7070/
     ProxyPassReverse / http://localhost:7070/
@@ -59,7 +63,7 @@ sudo a2ensite seguro.conf
 sudo systemctl reload apache2
 
 # Certificado SSL
-DOMINIO="suslinks.sus.codes"
+DOMINIO="arblocks.do"
 EMAIL="axrg0002@ce.pucmm.edu.do"
 
 sudo systemctl stop apache2
@@ -71,17 +75,13 @@ else
 fi
 sudo systemctl start apache2
 
-cd ~/ApacheReady/proyecto-final
+cd ~/ArSpring
+git pull
 chmod +x gradlew
 
 # 🔹 Ejecutar creación del fat JAR con la variable de entorno
-export MONGODB_URL="mongodb+srv://axrg0002:zKtiFY0qtxu9vdhy@finalpro.jhwraki.mongodb.net/?retryWrites=true&w=majority&appName=FinalPro"
 ./gradlew shadowjar
 
 # 🔹 Iniciar la app en segundo plano con la variable de entorno
-nohup env MONGODB_URL="mongodb+srv://axrg0002:zKtiFY0qtxu9vdhy@finalpro.jhwraki.mongodb.net/?retryWrites=true&w=majority&appName=FinalPro" \
-     java -jar build/libs/app.jar > build/libs/salida.txt 2> build/libs/error.txt &
+java -jar build/libs/app.jar > build/libs/salida.txt 2> build/libs/error.txt &
 
-# 🔹 Asegurar puerto 50051 esté accesible (gRPC)
-echo "Asegurando acceso al puerto 50051 para gRPC..."
-sudo ufw allow 50051/tcp  # O equivalente para tu entorno si no usas UFW
