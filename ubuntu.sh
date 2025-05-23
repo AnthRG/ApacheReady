6f9921d1d82fa0896fd3d0a9a802138b3b41094b
Anthony Rosario G. <112951262+AnthRG@users.noreply.github.com>
2025-05-23 11:52:49 -0400

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -73,7 +73,36 @@ if [ ! -f "/etc/letsencrypt/live/$DOMINIO/cert.pem" ]; then
 else
     echo "Certificado para $DOMINIO ya existe."
 fi
+
+# Configuración de VirtualHost (AFTER certs exist)
+sudo bash -c 'cat > /etc/apache2/sites-available/seguro.conf << EOF
+<VirtualHost *:80>
+    ServerName arblocks.do
+    Redirect permanent / https://arblocks.do
+</VirtualHost>
+
+<VirtualHost *:443>
+    ServerName arblocks.do
+
+    DocumentRoot /var/www/html
+
+    SSLEngine on
+    SSLCertificateFile /etc/letsencrypt/live/arblocks.do/fullchain.pem
+    SSLCertificateKeyFile /etc/letsencrypt/live/arblocks.do/privkey.pem
+
+    ProxyPass / http://localhost:8080/
+    ProxyPassReverse / http://localhost:8080/
+
+    <Directory "/var/www/html">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF'
+
+sudo a2ensite seguro.conf
 sudo systemctl start apache2
+sudo systemctl reload apache2
 
 cd ~/ArSpring
 git pull
@@ -82,6 +111,5 @@ chmod +x gradlew
 # 🔹 Ejecutar creación del fat JAR con la variable de entorno
 ./gradlew bootJar
 
-# 🔹 Iniciar la app en segundo plano con la variable de entorno
-java -jar build/libs/app.jar
-
+# Run the Spring Boot app (ensure port matches Apache Proxy)
+java -jar build/libs/app.jar > build/libs/salida.txt 2> build/libs/error.txt &
